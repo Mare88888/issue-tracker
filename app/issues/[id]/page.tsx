636de221360ec396d30fe9f2a5b7824b352522d0
Marko Lempl <marko.lempl4@gmail.com>
2026-01-28 20:52:26 +0100

    Fixed show issue details

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,5 @@
 import prisma from '@/prisma/client';
+import { Heading } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
@@ -7,9 +8,6 @@ interface Props {
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-    if (typeof (await params).id !== "number") {
-        notFound();
-    }
 
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt((await params).id) }
@@ -22,7 +20,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 
     return (
         <div>
-            <p>{issue.title}</p>
+            <Heading>{issue.title}</Heading>
             <p>{issue.description}</p>
             <p>{issue.status}</p>
             <p>{issue.createdAt.toDateString()}</p>
