5c8a8c63e223553a53d850b2b6dfb315e799286a
Marko Lempl <marko.lempl4@gmail.com>
2026-01-28 21:19:22 +0100

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Button, Table } from '@radix-ui/themes'
-import Link from 'next/link'
+import { Table } from '@radix-ui/themes'
+import Link from '../components/Link';
 import prisma from '@/prisma/client';
 import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
 import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
@@ -24,8 +23,9 @@ const IssuesPage = async () => {
                     {issues.map(issue => (
                         <Table.Row key={issue.id}>
                             <Table.Cell>
-                                <Link href={`/issues/${issue.id}`}
-                                >{issue.title}</Link>
+                                <Link href={`/issues/${issue.id}`}>
+                                    {issue.title}
+                                </Link>
 
                                 <div className="block md:hidden">
                                     <IssueStatusBadge status={issue.status} />
