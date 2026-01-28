85803d3ef4ff69f79307262295f3c2d3ad75f058
Marko Lempl <marko.lempl4@gmail.com>
2026-01-28 15:39:30 +0100

    Extract the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from '@/app/validationSchema';
 import { z } from 'zod';
 import { Text } from '@radix-ui/themes';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
 
@@ -45,9 +46,9 @@ const NewIssuePage = () => {
 
                     </TextField.Slot>
                 </TextField.Root>
-                {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
                 <Controller name="description" control={control} render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />
-                {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
                 <Button>Submit New Issue</Button>
             </form>
         </div>
