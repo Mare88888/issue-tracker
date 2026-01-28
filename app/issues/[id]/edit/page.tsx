784221f1f48a597561012c819fd448f63ae6d0bf
Marko Lempl <marko.lempl4@gmail.com>
2026-01-29 00:30:30 +0100

    Improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,6 +1,10 @@
 import prisma from '@/prisma/client';
-import IssueForm from '../../_components/IssueForm'
+import dynamic from 'next/dynamic';
 import { notFound } from 'next/navigation';
+
+const IssueForm = dynamic(
+    () => import('@/app/issues/_components/IssueForm'),
+)
 
 interface Props {
     params: Promise<{ id: string }>;
