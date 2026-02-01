0dc477147eebf91cea9a5b92c26a86acccf6b0d0
Marko Lempl <marko.lempl4@gmail.com>
2026-02-01 22:49:27 +0100

    Refactor dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,15 @@ export default async function Home() {
   const open = await prisma.issue.count({ where: { status: Status.OPEN } })
   const inProgress = await prisma.issue.count({ where: { status: Status.IN_PROGRESS } })
   const closed = await prisma.issue.count({ where: { status: Status.CLOSED } })
+  const statusCounts = { open, inProgress, closed }
 
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Flex direction="column" gap="5">
-        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
-        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        <IssueSummary {...statusCounts} />
+        <IssueChart {...statusCounts} />
       </Flex>
       <LatestIssues />
     </Grid>
-
   );
 }
